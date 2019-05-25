977cb25239ba1bb72ae95d1439bcd75528b450d0
Alexander Hartson <alexander-hartson@live.com>
2019-05-25 13:49:28 -0500

    setting up express.js security

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,34 @@
 import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 
-app.get('*', (req, res) => res.send('Hello World!'));
+// Setup root of project
+const root = path.join(__dirname, '../../');
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", 'data:', '*.amazonaws.com'],
+  },
+}));
+
+app.use(compress());
+app.use(cors());
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+
+// Serve static files dist/client
+app.use('/', express.static(path.join(root, 'dist/client')));
+// Serve avater images under uploads
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+// Set initial path and catch any request
+// Transfer the file at the given path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
+});
 app.listen(8000, () => console.log('Listening on port 8000!'));
