85b190f4801291e2f7b72d2e3b0fba80cd1d1b9d
Alexander Hartson <alexander-hartson@live.com>
2019-05-25 21:15:59 -0500

    setting up graphQL

--- src/server/index.js
+++ src/server/index.js
@@ -1,31 +1,46 @@
+// Express.js server
+
 import express from 'express';
 import path from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
-
-const app = express();
+import services from './services';
 
 // Setup root of project
 const root = path.join(__dirname, '../../');
+const app = express();
 
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    styleSrc: ["'self'", 'data:', '*.amazonaws.com'],
-  },
-}));
+if (process.env.NODE_ENV === 'production') {
+  app.use(helmet());
+  app.use(helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      styleSrc: ["'self'", 'data:', '*.amazonaws.com'],
+    },
+  }));
+  app.use(compress());
+  app.use(cors());
+}
 
-app.use(compress());
-app.use(cors());
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
-
 // Serve static files dist/client
 app.use('/', express.static(path.join(root, 'dist/client')));
 // Serve avater images under uploads
 app.use('/uploads', express.static(path.join(root, 'uploads')));
+
+// Bind GraphQL server to the Express.js web server
+const serviceNames = Object.keys(services);
+for (let i = 0; i < serviceNames.length; i += 1) {
+  const name = serviceNames[i];
+  if (name === 'graphql') {
+    services[name].applyMiddleware({ app });
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
+
 // Set initial path and catch any request
 // Transfer the file at the given path
 app.get('/', (req, res) => {
---
+++ src/server/services/index.js
@@ -0,0 +1,7 @@
+// GraphQL server
+
+import graphql from './graphql';
+
+export default {
+  graphql,
+};
