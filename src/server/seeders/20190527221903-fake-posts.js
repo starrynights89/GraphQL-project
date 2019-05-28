cda9500a3dd0fef393d83ea428c3728739b037ec
Alexander Hartson <alexander-hartson@live.com>
2019-05-27 19:12:46 -0500

    building SQL migration

---
+++ src/server/seeders/20190527221903-fake-posts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Posts', [{
+      text: 'Lorem ipsum 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Posts', null, {});
+  },
+};
