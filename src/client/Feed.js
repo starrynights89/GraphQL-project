a8006846cae2a4b9c9963604551374b05b49d9da
Alexander Hartson <alexander-hartson@live.com>
2019-06-03 22:04:25 -0500

    more building of feed

--- src/client/Feed.js
+++ src/client/Feed.js
@@ -54,7 +54,7 @@ export default class Feed extends Component {
     const { postContent } = this.state;
     
     return (
-      <Query query={GET_POSTS}>
+      <Query query={GET_POSTS} pollInterval={5000}>
         {({ loading, error, data }) => {
           if (loading) return "Loading...";
           if (error) return error.message;
@@ -69,6 +69,19 @@ export default class Feed extends Component {
                     const data = store.readQuery({ query: GET_POSTS });
                     data.posts.unshift(addPost);
                     store.writeQuery({ query: GET_POSTS, data });
+                  }}
+                  optimisticResponse = {{
+                    __typename: "mutation",
+                    addPost: {
+                      __typename: "Post",
+                      text: postContent,
+                      id: -1,
+                      user: {
+                        __typename: "User",
+                        username: "Loading...",
+                        avatar: "/public/loading.gif"
+                      }
+                    }
                   }}
                 >
                   {addPost => (
@@ -92,7 +105,7 @@ export default class Feed extends Component {
               </div>
               <div className="feed">
                 {posts.map((post, i) =>
-                  <div key={post.id} className="post">
+                  <div key={post.id} className={'post ' + (post.id < 0 ? 'optimistic': '')}>
                     <div className="header">
                       <img src={post.user.avatar} />
                       <h2>{post.user.username}</h2>
