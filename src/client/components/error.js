fd45a9c95bd90bdfdc967b37d3252684603661a1
Alexander Hartson <alexander-hartson@live.com>
2019-06-07 22:12:27 -0500

    adding first components

---
+++ src/client/components/error.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+export default class Error extends Component {
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div className="error message">
+        {children}
+      </div>
+    );
+  }
+}
