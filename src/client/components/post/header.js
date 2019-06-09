6160620b2a560dc0d78af83e4c5c60149fec380e
Alexander Hartson <alexander-hartson@live.com>
2019-06-08 20:49:39 -0500

    Breaking up Feed.js

---
+++ src/client/components/post/header.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+export default ({ post }) => 
+  <div className="header">
+    <img src={post.user.avatar} />
+    <div>
+      <h2>{post.user.username}</h2>
+    </div>
+  </div>
