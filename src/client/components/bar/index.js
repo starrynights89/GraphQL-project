9369026a3732c7f00cf721784526a73a57c6daa8
Alexander Hartson <alexander-hartson@live.com>
2019-06-13 11:32:34 -0500

    Adding user login status to bar component

--- src/client/components/bar/index.js
+++ src/client/components/bar/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import SearchBar from './search';
+import { UserConsumer } from '../context/user';
+import UserBar from './user';
 
 export default class Bar extends Component {
   render() {
@@ -7,6 +9,9 @@ export default class Bar extends Component {
       <div className="topbar">
         <div className="inner">
           <SearchBar />
+          <UserConsumer>
+            <UserBar />
+          </UserConsumer>
         </div>
       </div>
     );
