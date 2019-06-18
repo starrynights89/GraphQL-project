178899628ac3deb5aa9ea2871db371ccf052fd71
Alexander Hartson <alexander-hartson@live.com>
2019-06-18 18:57:03 -0500

    Revert "Adding email login. Password auth not setup yet"
    This reverts commit 36777658a429643be4d25357722ef54bdf0fc886.

--- src/client/App.js
+++ src/client/App.js
@@ -5,24 +5,8 @@ import Chats from './Chats';
 import '../../assets/css/style.css';
 import './components/fontawesome';
 import Bar from './components/bar';
-import LoginRegisterForm from './components/loginregister';
 
 export default class App extends Component {
-  state = {
-    loggedIn: false
-  }
-
-  componentWillMount() {
-    const token = localStorage.getItem('jwt');
-    if(token) {
-      this.setState({loggedIn: true});
-    }
-  }
-
-  changeLoginState = (loggedIn) => {
-    this.setState({ loggedIn });
-  }
-
   render() {
     return (
       <div className="container">
@@ -30,14 +14,9 @@ export default class App extends Component {
           <title>Graphbook - Feed</title>
           <meta name="description" content="Newsfeed of all your friends on Graphbook" />
         </Helmet>
-        {this.state.loggedIn ? 
-            <div>
-              <Bar />
-              <Feed />
-              <Chats />
-            </div>
-            : <LoginRegisterForm changeLoginState={this.changeLoginState} />
-        }
+        <Bar />
+        <Feed />
+        <Chats />
       </div>
     );
   }
