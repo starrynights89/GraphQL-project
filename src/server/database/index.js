20fcca4e1c877d364ef6dbd43c2958853363781b
Alexander Hartson <alexander-hartson@live.com>
2019-07-13 03:41:51 -0500

    Adding new node.js packages

--- src/server/database/index.js
+++ src/server/database/index.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import configFile from '../config/';
+import configFile from '../config';
 import models from '../models';
 
 const env = process.env.NODE_ENV || 'development';
