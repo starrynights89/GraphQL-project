e5c082f8f40c5def562d5f74a0c655bf2d1f6df7
Alexander Hartson <alexander-hartson@live.com>
2019-07-14 11:51:35 -0500

    Adding SSR folder

---
+++ src/server/ssr/app.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import { withApollo } from 'react-apollo';
+import '../../client/components/fontawesome';
+import Router from '../../client/router';
+
+class App extends Component{
+  state = {
+    loggedIn: false
+  }
+  changeLoginState = (loggedIn) => {
+    this.setState({ loggedIn });
+  }
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Graphsite - Feed</title>
+          <meta name="description" content="Newsfeed of all your
+            friends on Graphbook" />
+        </Helmet>
+        <Router loggedIn={this.state.loggedIn} changeLoginState=
+          {this.changeLoginState} location={this.props.location}
+          content={this.props.context} />
+      </div>
+    )
+  }
+} 
+
+export default withApollo(App)
