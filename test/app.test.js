ace695b2a2d4cdf25d33bd5df35831ccd446f62b
Alexander Hartson <alexander-hartson@live.com>
2019-08-06 22:29:09 -0500

    Finish writing tests for graphQL. More bugs to test

--- test/app.test.js
+++ test/app.test.js
@@ -7,7 +7,8 @@ const should = require('chai').should();
 require('babel-plugin-require-context-hook/register')();
 
 describe('Graphbook application test', () => {
-  var app; 
+  var app;
+  var authToken;
 
   before((done) => {
     app = require('../src/server').default;
@@ -33,7 +34,7 @@ describe('Graphbook application test', () => {
   describe('404', () => {
     it('redirects the user when not matching path is found', (done) => {
       request({
-        url: 'http://localhost:8000',
+        url: 'http://localhost:8000/',
       }, (err, res, body) => {
         should.not.exist(err);
         should.exist(res);
@@ -57,6 +58,55 @@ describe('Graphbook application test', () => {
         assert.ok(res.req.path === '/');
         assert.ok(body.indexOf('<html') !== -1);
         assert.ok(body.indexOf('class="authModal"') !== -1);
+        done(err);
+      });
+    });
+
+    it('allows the user to sign up', (done) => {
+      const json = {
+        operationName: null,
+        query: 'mutation signup($username: String!, $email : String!, $password : String!) { signup(username: $username, email: $email, password : $password) { token }}',
+        variables: {
+          email: 'mocha@test.com',
+          username: 'mochatest',
+          password: '123456789',
+        },
+      };
+
+      request.post({
+        url: 'http://localhost:8000/graphql',
+        json,
+      }, (err, res, body) => {
+        should.not.exist(err);
+        should.exist(res);
+        expect(res.statusCode).to.be.equal(200);
+        body.should.be.an('object');
+        body.should.have.property('data');
+        authToken = body.data.signup.token;
+        done(err);
+      });
+    });
+
+    it('allows the user to query all chats', (done) => {
+      const json = {
+        operationName: null,
+        query: 'query {chats {id users {id avatar username}}}',
+        variables: {},
+      };
+
+      request.post({
+        url: 'http://localhost:8000/graphql',
+        headers: {
+          Authorization: authToken,
+        },
+        json,
+      }, (err, res, body) => {
+        should.not.exist(err);
+        should.exist(res);
+        expect(res.statusCode).to.be.equal(200);
+        body.should.be.an('object');
+        body.should.have.property('data');
+        body.data.should.have.property('chats').with.lengthOf(0);
         done(err);
       });
     });
