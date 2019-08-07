7b357b701224195b25cd1cca35594665ff32d4dc
Alexander Hartson <alexander-hartson@live.com>
2019-08-06 20:57:13 -0500

    Adding 404 and authentication

--- test/app.test.js
+++ test/app.test.js
@@ -29,4 +29,36 @@ describe('Graphbook application test', () => {
       done(err);
     });
   });
+
+  describe('404', () => {
+    it('redirects the user when not matching path is found', (done) => {
+      request({
+        url: 'http://localhost:8000',
+      }, (err, res, body) => {
+        should.not.exist(err);
+        should.exist(res);
+        expect(res.statusCode).to.be.equal(200);
+        assert.ok(res.req.path === '/');
+        assert.ok(body.indexOf('<html') !== -1);
+        assert.ok(body.indexOf('class="authModal"') !== -1);
+        done(err);
+      });
+    });
+  });
+
+  describe('authentication', () => {
+    it('redirects the user when not logged in', (done) => {
+      request({
+        url: 'http://localhost:8000/app',
+      }, (err, res, body) => {
+        should.not.exist(err);
+        should.exist(res);
+        expect(res.statusCode).to.be.equal(200);
+        assert.ok(res.req.path === '/');
+        assert.ok(body.indexOf('<html') !== -1);
+        assert.ok(body.indexOf('class="authModal"') !== -1);
+        done(err);
+      });
+    });
+  });
 });
